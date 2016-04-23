3ab2b4df871911003aa6dadb8afda187c4c292b2
Til Blechschmidt <til@blechschmidt.de>
2016-04-23 15:58:17 +0200

    Changed test

--- src/structs/structs.rs
+++ src/structs/structs.rs
@@ -20,8 +20,8 @@ fn test_fade_curve() {
     let curve = FadeCurve::Custom("m * sin(x)".to_string());
     let timeframe = 1000.0; //ms
     for i in 1..255 {
-        let expr = curve.clone().to_expression(timeframe as FadeTime, i).unwrap();
-        let value = Some(timeframe).map(&*expr).unwrap();
+        let expr = &*curve.clone().to_expression(timeframe as FadeTime, i).unwrap();
+        let value = expr(timeframe);//Some(timeframe).map(&*expr).unwrap();
         assert_eq!(value as DmxValue, i as DmxValue);
     }
 }
