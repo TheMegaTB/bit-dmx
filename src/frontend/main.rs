489d4f1517472cbb9ad1feb1b1d1f47101d7f4f4
Til Blechschmidt <til@blechschmidt.de>
2016-04-24 20:50:59 +0200

    Renamed watchdog_server -> watchdog

--- src/frontend/main.rs
+++ src/frontend/main.rs
@@ -3,8 +3,8 @@ use structures::*;
 
 fn main() {
     let socket = UDPSocket::new();
-    let watchdog_server = socket.start_watchdog_client();
+    let watchdog = socket.start_watchdog_client();
     std::thread::sleep(std::time::Duration::from_secs(10));
-    println!("{}", watchdog_server.is_alive());
-    println!("{:?}", watchdog_server.get_server_addr());
+    println!("{}", watchdog.is_alive());
+    println!("{:?}", watchdog.get_server_addr());
 }
