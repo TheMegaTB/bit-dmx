f2a98bd96ff6f1124b6a9c20472fda2d06af4d15
Til Blechschmidt <til@blechschmidt.de>
2016-04-24 16:56:32 +0200

    Moved imports into test block

--- src/backend/main.rs
+++ src/backend/main.rs
@@ -2,30 +2,9 @@ extern crate structures;
 
 mod interface_handler;
 
-use interface_handler::*;
-use structures::*;
-use std::sync::mpsc;
-
 fn main() {
 
 }
-
-// pub fn connect_and_test() {
-//     let interface = Interface::new().connect();
-//     if interface.is_err() { panic!(interface) }
-//     let (tx, interrupt_tx) = interface.unwrap().to_thread();
-//     (0..255).chain((0..255).rev()).map(|i| {
-//         tx.send((1, i)).unwrap();
-//         tx.send((2, i)).unwrap();
-//         //tx.send((3, i)).unwrap();
-//         sleep(Duration::from_millis(16));
-//         // interface.write_to_dmx(1, i);
-//         // println!("{}", i);
-//     }).collect::<Vec<_>>();
-//     sleep(Duration::from_millis(2000));
-//     println!("Disconnecting...");
-//     interrupt_tx.send(true).unwrap();
-// }
 
 
 #[test]
@@ -33,6 +12,8 @@ fn main() {
 fn test_fade_curve() {
     use std::time::Duration;
     use std::thread::sleep;
+    use interface_handler::*;
+    use structures::*;
 
     let interface = Interface::new().connect();
     if interface.is_err() { panic!(interface) }
