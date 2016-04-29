5fcce5699deccdfd6a65e04c2909858b159c3fdc
Til Blechschmidt <til@blechschmidt.de>
2016-04-29 20:37:51 +0200

    Removed all scenes

--- src/frontend/main.rs
+++ src/frontend/main.rs
@@ -46,29 +46,6 @@ widget_ids! {
 }
 
 fn create_output_window(tx: mpsc::Sender<Vec<u8>>) {
-    // let mut window: PistonWindow =
-    //     WindowSettings::new("Canvas Demo", [800, 600])
-    //         .exit_on_esc(true).vsync(true).build().unwrap();
-    //
-    // // construct our `Ui`.
-    // let mut ui = {
-    //     let assets = find_folder::Search::ParentsThenKids(3, 3)
-    //         .for_folder("assets").unwrap();
-    //     let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
-    //     let theme = Theme::default();
-    //     let glyph_cache = Glyphs::new(&font_path, window.factory.clone());
-    //     Ui::new(glyph_cache.unwrap(), theme)
-    // };
-    //
-    // window.set_ups(60);
-    //
-    // // Poll events from the window.
-    // while let Some(event) = window.next() {
-    //     ui.handle_event(&event);
-    //     // event.update(|_| ui.set_widgets(set_widgets));
-    //     window.draw_2d(&event, |c, g| ui.draw_if_changed(c, g));
-    // }
-
     let mut window: PistonWindow = WindowSettings::new("Sushi Reloaded!", [1100, 560])
                                     .exit_on_esc(true).vsync(true).build().unwrap();
 
@@ -82,31 +59,6 @@ fn create_output_window(tx: mpsc::Sender<Vec<u8>>) {
     };
 
     let mut buttons: Vec<(usize, bool, String)> = Vec::new();
-    buttons.push((0, false, "Umbaulight".to_string()));
-    buttons.push((1, false, "Scene 1.1".to_string()));
-    buttons.push((2, false, "Scene 1.2".to_string()));
-    buttons.push((3, false, "Scene 2.1".to_string()));
-    buttons.push((4, false, "Scene 2.2".to_string()));
-    buttons.push((5, false, "Scene 2.3".to_string()));
-    buttons.push((6, false, "Scene 3.1".to_string()));
-    buttons.push((7, false, "Scene 3.2".to_string()));
-    buttons.push((8, false, "Scene 3.3".to_string()));
-    buttons.push((9, false, "Scene 4.1".to_string()));
-    buttons.push((10, false, "Scene 5.1".to_string()));
-    buttons.push((11, false, "Scene 5.2".to_string()));
-    buttons.push((12, false, "Scene 5.3".to_string()));
-    buttons.push((13, false, "Scene 6 (2x)".to_string()));
-    buttons.push((14, false, "Scene 7".to_string()));
-    buttons.push((15, false, "Scene 8.1".to_string()));
-    buttons.push((16, false, "Scene 8.2".to_string()));
-    buttons.push((17, false, "Scene 15.1".to_string()));
-    buttons.push((18, false, "Scene 15.2".to_string()));
-
-    // for event in window.ups(60) {
-    //     ui.handle_event(&event);
-    //     event.update(|_| ui.set_widgets(|mut ui| set_widgets(&mut ui, &mut buttons, tx.clone())));
-    //     event.draw_2d(|c, g| ui.draw_if_changed(c, g));
-    // }
 
     window.set_ups(60);
 
@@ -128,45 +80,45 @@ fn set_widgets(mut ui: &mut UiCell, buttons: &mut Vec<(usize, bool, String)>, tx
         .scroll_kids()
         .set(CANVAS, &mut ui);
 
-    Text::new("Widget Demonstration")
+    Text::new("Moonshadow 2016!")
         .top_left_with_margin_on(CANVAS, 0.0)
         .font_size(32)
         .color(bg_color.plain_contrast())
         .set(TITLE, ui);
 
-    let mut i = 0;
-    let mut id = TITLE;
-    for button in buttons.iter_mut() {
-        Button::new()
-            .w_h(200.0, 50.0)
-            .and(|b| {
-                if i > 0 {
-                    b.right_from(id, 5.0)
-                } else { b.down(25.0) }
-            })
-            .and(|b| {
-                if button.1 {
-                    b.rgb(0.1, 0.9, 0.1)
-                } else {
-                    b.rgb(0.9, 0.1, 0.1)
-                }
-            })
-            .frame(1.0)
-            .label(&(button.2.clone()))
-            .react(|| {
-                button.1 = !button.1;
-                if button.1 {
-                    tx.send(vec![1, 0, button.0 as u8, 255]).unwrap()
-                    // client.send(&[1, 0, button.0 as u8, 255], server);
-                } else {
-                    tx.send(vec![1, 0, button.0 as u8, 0]).unwrap()
-                    // client.send(&[1, 0, button.0 as u8, 0], server);
-                }
-            })
-            .set(BUTTON + i, ui);
-        id = BUTTON + i;
-        i += 1;
-    }
+    // let mut i = 0;
+    // let mut id = TITLE;
+    // for button in buttons.iter_mut() {
+    //     Button::new()
+    //         .w_h(200.0, 50.0)
+    //         .and(|b| {
+    //             if i > 0 {
+    //                 b.right_from(id, 5.0)
+    //             } else { b.down(25.0) }
+    //         })
+    //         .and(|b| {
+    //             if button.1 {
+    //                 b.rgb(0.1, 0.9, 0.1)
+    //             } else {
+    //                 b.rgb(0.9, 0.1, 0.1)
+    //             }
+    //         })
+    //         .frame(1.0)
+    //         .label(&(button.2.clone()))
+    //         .react(|| {
+    //             button.1 = !button.1;
+    //             if button.1 {
+    //                 tx.send(vec![1, 0, button.0 as u8, 255]).unwrap()
+    //                 // client.send(&[1, 0, button.0 as u8, 255], server);
+    //             } else {
+    //                 tx.send(vec![1, 0, button.0 as u8, 0]).unwrap()
+    //                 // client.send(&[1, 0, button.0 as u8, 0], server);
+    //             }
+    //         })
+    //         .set(BUTTON + i, ui);
+    //     id = BUTTON + i;
+    //     i += 1;
+    // }
 }
 
 fn main() {
@@ -192,11 +144,6 @@ fn main() {
             }
         });
 
-        //let output_thread = thread::spawn(move || {
-            println!("Hello world from output window thread!");
-            create_output_window(tx);
-        //});
-
-        //output_thread.join().unwrap();
+        create_output_window(tx);
     }
 }
