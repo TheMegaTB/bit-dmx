a3b3692d0bd7d7458c05b34998db4f469bcbb573
Til Blechschmidt <til@blechschmidt.de>
2016-05-29 00:43:10 +0200

    Removed old splash function

--- src/frontend/main.rs
+++ src/frontend/main.rs
@@ -853,25 +853,6 @@ fn get_start_chaser(shift_state: bool, addr: u16, value: u8) -> Vec<u8> {
     vec![if shift_state {130} else {2}, addr_high, addr_low, value]
 }
 
-// fn create_splash_window(ui: Arc<Mutex<UI>>) {
-//     let (mut window, mut conrod_ui) = create_window("BitDMX Splashscreen".to_string(), (500, 300), 1, true);
-//
-//     while let Some(event) = window.next() {
-//         conrod_ui.handle_event(&event);
-//         window.draw_2d(&event, |c, g| conrod_ui.draw(c, g));
-//
-//         event.update(|_| conrod_ui.set_widgets(|mut conrod_ui| {
-//             Canvas::new()
-//                 .frame(1.0)
-//                 .pad(30.0)
-//                 .color(color::rgb(0.236, 0.239, 0.900))
-//                 .set(CANVAS, &mut conrod_ui);
-//         }));
-//
-//         if ui.lock().unwrap().watchdog.is_alive() { break }
-//     }
-// }
-
 
 fn main() {
     init_logger().unwrap();
