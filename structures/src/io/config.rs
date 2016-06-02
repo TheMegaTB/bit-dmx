510b2b4e3e87134c264a9145bcf220a31bd12ac0
Til Blechschmidt <til@blechschmidt.de>
2016-06-03 00:22:38 +0200

    Creating config folder if it doesn't exist

--- structures/src/io/config.rs
+++ structures/src/io/config.rs
@@ -1,6 +1,8 @@
 use std::path::PathBuf;
 use find_folder;
 
+use std::error::Error;
+
 pub enum Config {
     Server,
     Client
@@ -14,5 +16,22 @@ pub fn get_config_path(conf: Config, name: &String) -> PathBuf {
         Config::Server => config_path.join("server").join(name),
         Config::Client => config_path.join("client")
     };
+    if match config_path.metadata() {
+        Ok(m) => {
+            if m.is_dir() { false } else { true }
+        },
+        Err(_) => {
+            warn!("No config directory found.");
+            true
+        }
+    } {
+        match ::std::fs::create_dir_all(&config_path) {
+            Ok(_) => {
+                info!("Created config directory @ {:?}", config_path);
+            }, Err(e) => {
+                exit!(8, "Unable to create config files: {}", e.description());
+            }
+        }
+    }
     config_path
 }
