17c1482d53dd41af30e6036f2faad1ca30558652
Jonas <jonas.it@icloud.com>
2016-06-03 14:35:49 +0200

    Removed thread from fixture editor so that it works on OSX too

--- src/fixture_editor/main.rs
+++ src/fixture_editor/main.rs
@@ -17,48 +17,48 @@ widget_ids! {
 }
 
 pub struct FixtureWindow {
-    thread: JoinHandle<()>
+
 }
 
 impl FixtureWindow {
+    pub fn start() {
+        let (mut window, mut conrod_ui) = match create_window("BitDMX Fixture editor".to_string(), (500, 300), 30, true) {
+            Ok(res) => res, Err(e) => {exit!(3, e);}
+        };
+
+        while let Some(event) = window.next() {
+            conrod_ui.handle_event(&event);
+            window.draw_2d(&event, |c, g| conrod_ui.draw(c, g));
+
+            event.update(|_| conrod_ui.set_widgets(|mut conrod_ui| {
+                // Move this into a function and possibly sub-file please...
+                Canvas::new()
+                    .frame(0.0)
+                    .pad(5.0)
+                    .color(FlatColor::concrete())
+                    .set(CANVAS, &mut conrod_ui);
+
+                Text::new("Hi!")
+                    // .w_h(110.0, 110.0)
+                    .middle_of(CANVAS)
+                    .font_size(100)
+                    .color(FlatColor::clouds())
+                    .set(HELLO_WORLD, &mut conrod_ui);
+            }));
+        };
+    }
+
     pub fn new() -> FixtureWindow {
-        FixtureWindow {
-            thread: thread::spawn(move || {
-                let (mut window, mut conrod_ui) = match create_window("BitDMX Fixture editor".to_string(), (500, 300), 30, true) {
-                    Ok(res) => res, Err(e) => {exit!(3, e);}
-                };
-
-                while let Some(event) = window.next() {
-                    conrod_ui.handle_event(&event);
-                    window.draw_2d(&event, |c, g| conrod_ui.draw(c, g));
-
-                    event.update(|_| conrod_ui.set_widgets(|mut conrod_ui| {
-                        // Move this into a function and possibly sub-file please...
-                        Canvas::new()
-                            .frame(1.0)
-                            .pad(5.0)
-                            .color(FlatColor::turquoise())
-                            .set(CANVAS, &mut conrod_ui);
-
-                        Text::new("Hi!")
-                            // .w_h(110.0, 110.0)
-                            .middle_of(CANVAS)
-                            .font_size(100)
-                            .color(FlatColor::green_sea())
-                            .set(HELLO_WORLD, &mut conrod_ui);
-                    }));
-                };
-            })
-        }
+        FixtureWindow {}
     }
 }
 
-impl DMXWindow for FixtureWindow {
-    fn join(self) -> Result<(), Box<Any + Send + 'static>> {
-        self.thread.join()
-    }
-}
+// impl DMXWindow for FixtureWindow {
+//     fn join(self) -> Result<(), Box<Any + Send + 'static>> {
+//         self.thread.join()
+//     }
+// }
 
 fn main() {
-    FixtureWindow::new().join().unwrap();
+    FixtureWindow::start();
 }
